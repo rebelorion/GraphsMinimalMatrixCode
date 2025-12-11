3f387e94b6a801a7874079644d8731f9ff42ed80
rebelorion <160419663+rebelorion@users.noreply.github.com>
2025-12-11 22:04:16 +0400

    функция для подсчета минимального матричного кода (пока только для graph6)

--- MinimalMatrixCode/Program.cs
+++ MinimalMatrixCode/Program.cs
@@ -97,13 +97,14 @@ class GraphReader
             return (i, posNumber);
         }
     
-        public void GetMinimalCode()
+        public int GetMinimalCode()
         {
             int[] colors = new int[N];
 
-            if (Matrix == null) return;
+            if (Matrix == null) return -1;
 
-            // раскрашиваем вершины по степеням соседей (инициализация)
+            int minimalVert = 0;
+            int minimalDegree = N + 1;
             for (int i = 0; i < N; i++)
             {
                 int sum = 0;
@@ -111,14 +112,37 @@ class GraphReader
                 {
                     sum += Matrix[i, j];
                 }
+                if (sum < minimalDegree)
+                {
+                    minimalDegree = sum;
+                    minimalVert = i;
+                }
                 colors[i] = sum;
             }
 
-            foreach (int color in colors)
+            // составляем массив элементов кроме вершины с минимальной
+            // степенью. Их и будем переставлять
+            List<int> others1 = new List<int>();
+            for (int i = 0; i < N; i++)
             {
-                Console.Write($"{color} | ");
+                if (i == minimalVert) continue;
+                others1.Add(i);
             }
-            
+            int[] others = others1.ToArray();
+
+            int minimalCode = int.MaxValue;
+            do // на каждой итерации генерируем перестановку
+            {
+                int tryCode = GetCode(minimalVert, others);
+                if (tryCode < minimalCode)
+                {
+                    minimalCode = tryCode;
+                }
+
+            } while (NextPermutation(others)); 
+
+            return minimalCode;
+
             // int[] newColors = new int[N]; // по умолчанию заполнен нулями
             // while (!newColors.SequenceEqual(colors)) // поэлементное сравнение
             // {
@@ -148,7 +172,51 @@ class GraphReader
             // теперь можем делать перестановки этих разбиений
             
         }
-    
+
+        // вычисляет код на основе перестановки
+        int GetCode(int lead, int[] permutation)
+        {
+            if (Matrix == null) return 0;
+
+            int result = 0;
+            int exponent = 1;
+            for (int i = N-1; i >= 0; i--)
+            {
+                for (int j = N-1; j > i; j--)
+                {
+                    // получаем число в матрице (0 или 1)
+                    // j - 1 так как у нас массив permutations не содержит lead вершину
+                    int multiplier = i != 0 ? 
+                        Matrix[permutation[i - 1], permutation[j - 1]] : // то есть если мы смотрим чисто по массиву permutation
+                        Matrix[lead, permutation[j - 1]]; // или смотрим верхнюю строку, которая lead вершина
+                    result += multiplier * exponent;
+                    exponent *= 2;
+                }
+            }
+            return result;
+        }
+
+        static bool NextPermutation(int[] arr)
+        {
+            int i = arr.Length - 2;
+            while (i >= 0 && arr[i] >= arr[i + 1]) i--;
+            if (i < 0) return false;
+            
+            int j = arr.Length - 1;
+            while (arr[j] <= arr[i]) j--;
+            
+            Swap(arr, i, j);
+            Reverse(arr, i + 1, arr.Length - 1);
+            return true;
+        }
+        
+        static void Swap(int[] arr, int i, int j) => (arr[j], arr[i]) = (arr[i], arr[j]);
+        
+        static void Reverse(int[] arr, int start, int end)
+        {
+            while (start < end) Swap(arr, start++, end--);
+        }
+        
     }
 
     public void ReadGraphs(string filename) // функция чтения файла в объект GraphReader
@@ -175,7 +243,10 @@ class Program
         GraphReader graphReader = new GraphReader(); // инициализируем
         graphReader.ReadGraphs(filename); // считываем графы
 
-        graphReader.Graph6![3].GetMinimalCode();
+        int minimalCode = graphReader.Graph6![1].GetMinimalCode();
+        Console.WriteLine(graphReader.Graph6[1]);
+
+        Console.WriteLine(minimalCode);
     }
 }
 
